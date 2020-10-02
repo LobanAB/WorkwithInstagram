856001efb1cb8583f2de948dcf3cb875fa55a609
Alex <48862843+LobanAB@users.noreply.github.com>
2020-10-02 10:22:44 +0300

    add compatibility with *nix

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -1,11 +1,12 @@
 import requests
 import utils
+from pathlib import Path
 
 
 def save_file(image, target_path, image_id):
     utils.make_dir(target_path)
     image_name = image.split("/")[-1]
-    filename = target_path + "/" + str(image_id) + image_name
+    filename = target_path.joinpath(str(image_id) + image_name)
     response = requests.get(image, verify=False)
     response.raise_for_status()
     with open(filename, 'wb') as file:
@@ -16,7 +17,7 @@ def fetch_hubble_image(image_id):
     api_url = "http://hubblesite.org/api/v3/image/%s" % image_id
     response = requests.get(api_url)
     response.raise_for_status()
-    save_file("http:" + response.json()["image_files"][1]["file_url"], "images/full_size", image_id)
+    save_file("http:" + response.json()["image_files"][1]["file_url"], Path.cwd() / 'images' / 'full_size', image_id)
 
 
 def fetch_hubble_collection(collection_name):
--- post_instagram.py
+++ post_instagram.py
@@ -4,35 +4,36 @@ from PIL import Image
 from instabot import Bot
 import time
 import utils
+from pathlib import Path
 
 
 def get_resize_image(path_to_image):
-    img_name = (path_to_image.split("/")[-1]).split(".")[0]
+    img_name = path_to_image.stem
     image = Image.open(path_to_image)
     image.thumbnail((1080, 1080))
-    utils.make_dir("images/thumbnail")
+    utils.make_dir(Path.cwd() / 'images' / 'thumbnail')
     if image.mode != "RGB":
         image = image.convert("RGB")
-    image.save("images/thumbnail/%s.jpg" % img_name, format="JPEG")
+    image.save(Path.cwd() / 'images' / 'thumbnail' / (img_name + '.jpg'), format="JPEG")
 
 
 def prepare_images_for_posting(folder):
     images = os.listdir(folder)
-    for image in images:
-        get_resize_image(folder + "/" + image)
+    for image_name in images:
+        get_resize_image(folder.joinpath(image_name))
 
 
 def main():
     load_dotenv()
     username = os.getenv("LOGIN")
     password = os.getenv("PASSWORD")
-    prepare_images_for_posting("images/full_size")
-    images_for_posting = os.listdir("images/thumbnail")
+    prepare_images_for_posting(Path.cwd() / 'images' / 'full_size')
+    images_for_posting = os.listdir(Path.cwd() / 'images' / 'thumbnail')
     timeout_in_seconds = 60
     bot = Bot()
     bot.login(username=username, password=password, use_cookie=True)
     for image in images_for_posting:
-        bot.upload_photo("images/thumbnail/" + image, caption=image)
+        bot.upload_photo(Path.cwd() / 'images' / ('thumbnail' + image), caption=image)
         time.sleep(timeout_in_seconds)
 
 
